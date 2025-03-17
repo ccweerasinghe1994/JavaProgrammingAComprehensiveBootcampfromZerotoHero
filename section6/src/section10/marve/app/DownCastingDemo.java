96c03e8712cd022fd5fc3155800a44cffc64d5d4
wchamara <chamara.weerasinghe@my.sliit.lk>
2025-03-17 13:14:01 +0530

    add DownCastingDemo to demonstrate upcasting and downcasting with Animal, Cat, and Dog classes

---
+++ section6/src/section10/marve/app/DownCastingDemo.java
@@ -0,0 +1,21 @@
+package section10.marve.app;
+
+import section10.marve.animals.Cat;
+import section10.marve.animals.Dog;
+import section10.marve.lbase.Animal;
+
+public class DownCastingDemo {
+
+    public static void main(String[] args) {
+
+        Animal animal;
+        Cat cat = new Cat();
+
+        animal = cat; // Upcasting
+        cat = (Cat) animal; // Downcasting
+
+        Dog dog = (Dog) animal;
+    }
+
+
+}
