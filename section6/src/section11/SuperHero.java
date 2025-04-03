720606fa351d9ee75f57742a97a9c5918a5d9ece
wchamara <chamara.weerasinghe@my.sliit.lk>
2025-04-03 09:05:59 +0530

    add SuperHero interface with methods for using powers and stopping villains

---
+++ section6/src/section11/SuperHero.java
@@ -0,0 +1,15 @@
+package section11;
+
+public interface SuperHero {
+    String usePower();
+
+    /**
+     * This method is used to stop a villain.
+     * if Y is passed, it will stop the villain.
+     * if N is passed, it will not stop the villain.
+     *
+     * @param a char
+     * @return String
+     */
+    String stopVillain(char a);
+}
